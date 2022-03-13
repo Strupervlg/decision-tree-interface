3d68f86ca04fef819f3863714b0db15f4b5948a7
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2022-03-13 18:02:23 +0300

    Добавлена функция создания элемента Действие

--- Plugin_draw_io/test_plugin.js
+++ Plugin_draw_io/test_plugin.js
@@ -1,18 +1,41 @@
 Draw.loadPlugin(function(ui) {
 
-    ui.menubar.addMenu('Test', function() {
-        alert("Hello")
-    });
+    var graph = ui.editor.graph;
+    var model = graph.getModel();
 
-
-    ui.sidebar.addPalette('Test', 'Tst', true, function(content) {
+    // Adds custom sidebar entry
+    ui.sidebar.addPalette('customElements', 'Custom elements', true, function(content) {
         content.appendChild(ui.sidebar.createVertexTemplate('rounded=1;whiteSpace=wrap;html=1;fillColor=#d5e8d4;strokeColor=#82b366;', 66, 30, "", "True"));
         content.appendChild(ui.sidebar.createVertexTemplate('rounded=1;whiteSpace=wrap;html=1;fillColor=#f8cecc;strokeColor=#b85450;', 66, 30, "", "False"));
     });
-
 
     var c = ui.sidebar.container;
     c.firstChild.click();
     c.insertBefore(c.lastChild, c.firstChild);
     c.insertBefore(c.lastChild, c.firstChild);
+
+
+    // Adds menu
+    ui.menubar.addMenu('Custom elements', function(menu, parent) {
+        ui.menus.addMenuItem(menu, 'actionElement');
+    });
+
+
+    // Adds resource for action
+    mxResources.parse('actionElement=Add action element');
+
+
+    // Adds action
+    ui.actions.addAction('actionElement', function() {
+        var theGraph = ui.editor.graph;
+        if(theGraph.isEnabled() && !theGraph.isCellLocked(theGraph.getDefaultParent())){
+          var pos=theGraph.getInsertPoint();
+          var newElement=new mxCell("",
+                    new mxGeometry(pos.x, pos.y, 120, 60),
+                    "rounded=1;whiteSpace=wrap;html=1;fontFamily=Helvetica;fontSize=12;");
+            newElement.value = "<font color=\"#6666ff\">Find </font><font color=\"#cccc00\">X</font>'s <font color=\"#00cccc\">left closest</font>&nbsp;<font color=\"#00cc00\">unused&nbsp;</font><br><font color=\"#ff66b3\">operand</font><br><font color=\"#cccc00\">A</font>";
+          newElement.vertex=!0;
+          theGraph.setSelectionCell(theGraph.addCell(newElement));
+        }
+    });
 });
