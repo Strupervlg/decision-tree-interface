1d30cb13c12c8a5cc9eba87b86ba7ccb0c8121e7
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2022-03-13 16:33:44 +0300

    Добавлены кастомные элементы True, False

--- Plugin_draw_io/test_plugin.js
+++ Plugin_draw_io/test_plugin.js
@@ -1,6 +1,18 @@
 Draw.loadPlugin(function(ui) {
 
     ui.menubar.addMenu('Test', function() {
-        alert("Hello");
+        alert("Hello")
     });
+
+
+    ui.sidebar.addPalette('Test', 'Tst', true, function(content) {
+        content.appendChild(ui.sidebar.createVertexTemplate('rounded=1;whiteSpace=wrap;html=1;fillColor=#d5e8d4;strokeColor=#82b366;', 66, 30, "", "True"));
+        content.appendChild(ui.sidebar.createVertexTemplate('rounded=1;whiteSpace=wrap;html=1;fillColor=#f8cecc;strokeColor=#b85450;', 66, 30, "", "False"));
+    });
+
+
+    var c = ui.sidebar.container;
+    c.firstChild.click();
+    c.insertBefore(c.lastChild, c.firstChild);
+    c.insertBefore(c.lastChild, c.firstChild);
 });
