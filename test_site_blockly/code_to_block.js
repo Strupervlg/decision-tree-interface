2d5e6cb573af5e7f46ff4c078e4b936f9bea0fc3
Moskalenko Nikita <53981215+Strupervlg@users.noreply.github.com>
2023-01-07 22:18:07 +0300

    Добавлен парсинг кода и перевод его в блоки и xml

---
+++ test_site_blockly/code_to_block.js
@@ -0,0 +1,361 @@
+function toBlock(rootNode) {
+	if (rootNode.stmt != null) {
+		stmtNodeToBlock(rootNode.stmt);
+	}
+}
+
+function stmtNodeToBlock(stmtNode) {
+    if(stmtNode.secondExpr == null) {
+        printExprNode(stmtNode.firstExpr);
+    } else if(stmtNode.secondExpr != null && stmtNode.firstExpr.type == ExprType.TREE_VAR) {
+        var assignment = new Blockly.BlockSvg(workspace, "assign_value_to_variable_decision_tree");
+        assignment.initSvg();
+        assignment.render();
+
+        var block1 = printExprNode(stmtNode.firstExpr);
+        assignment.getInput("ref_to_object").connection.connect(block1.outputConnection);
+
+        var block2 = printExprNode(stmtNode.secondExpr);
+        assignment.getInput("new_object").connection.connect(block2.outputConnection);
+
+    } else if(stmtNode.secondExpr != null && stmtNode.firstExpr.type == ExprType.PROPERTY) {
+        var assignment = new Blockly.BlockSvg(workspace, "assign_value_to_property");
+        assignment.initSvg();
+        assignment.render();
+
+        var objBlock = printExprNode(stmtNode.firstExpr.firstOperand);
+        assignment.getInput("object").connection.connect(objBlock.outputConnection);
+
+        var propBlock = new Blockly.BlockSvg(workspace, "property");
+        propBlock.initSvg();
+        propBlock.render();
+        propBlock.inputList[0].fieldRow[0].setValue(stmtNode.firstExpr.ident);
+        assignment.getInput("property").connection.connect(propBlock.outputConnection);
+
+        var valueBlock = printExprNode(stmtNode.secondExpr);
+        assignment.getInput("new_value").connection.connect(valueBlock.outputConnection);
+    }
+}
+
+function printExprNode(exprNode) {
+    switch(exprNode.type) {
+        case ExprType.ID:
+            //TODO: определение переменной и объекта
+            var resBlock = new Blockly.BlockSvg(workspace, "object");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.ident);
+            return resBlock
+        case ExprType.STRING:
+            var resBlock = new Blockly.BlockSvg(workspace, "string");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.string);
+            return resBlock
+        case ExprType.INT:
+            var resBlock = new Blockly.BlockSvg(workspace, "integer");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.int);
+            return resBlock
+        case ExprType.DOUBLE:
+            var resBlock = new Blockly.BlockSvg(workspace, "double");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.double);
+            return resBlock
+        case ExprType.BOOLEAN:
+            var resBlock = new Blockly.BlockSvg(workspace, "boolean");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.boolean);
+            return resBlock
+        case ExprType.TREE_VAR:
+            var resBlock = new Blockly.BlockSvg(workspace, "ref_to_decision_tree_var");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.ident.substring(4)); //FIXME: добавить это в парсер
+            return resBlock
+        case ExprType.ENUM:
+            var resBlock = new Blockly.BlockSvg(workspace, "enum");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[0].fieldRow[0].setValue(exprNode.ident);
+            resBlock.inputList[1].fieldRow[0].setValue(exprNode.identValue);
+            return resBlock
+        case ExprType.GET_BY_RELATIONSHIP:
+            var resBlock = new Blockly.BlockSvg(workspace, "get_relationship_object");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[3].fieldRow[0].setValue(exprNode.ident);
+
+            relBlock = new Blockly.BlockSvg(workspace, "relationship");
+            relBlock.initSvg();
+            relBlock.render();
+            relBlock.inputList[0].fieldRow[0].setValue(exprNode.rel);
+            resBlock.getInput("relationship").connection.connect(relBlock.outputConnection);
+            
+            objBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            boolBlock = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("boolean").connection.connect(boolBlock.outputConnection);
+
+            return resBlock
+        case ExprType.PROPERTY:
+            var resBlock = new Blockly.BlockSvg(workspace, "get_property_value");
+            resBlock.initSvg();
+            resBlock.render();
+
+            propBlock = new Blockly.BlockSvg(workspace, "property");
+            propBlock.initSvg();
+            propBlock.render();
+            propBlock.inputList[0].fieldRow[0].setValue(exprNode.ident);
+            resBlock.getInput("property").connection.connect(propBlock.outputConnection);
+            
+            objBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            return resBlock
+        case ExprType.IS:
+            var resBlock = new Blockly.BlockSvg(workspace, "check_object_class");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            objBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            if(exprNode.secondOperand.type == ExprType.ID) {
+                var classBlock = new Blockly.BlockSvg(workspace, "class");
+                classBlock.initSvg();
+                classBlock.render();
+                classBlock.inputList[0].fieldRow[0].setValue(exprNode.secondOperand.ident);
+            }
+            else {
+                var classBlock = printExprNode(exprNode.secondOperand);
+            }
+            resBlock.getInput("class").connection.connect(classBlock.outputConnection);
+            return resBlock
+        case ExprType.GREATER:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("GREATER");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.LESS:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("LESS");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.EQUAL:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("EQUAL");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.NOT_EQUAL:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("NOT_EQUAL");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.GE:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("GE");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.LE:
+            var resBlock = new Blockly.BlockSvg(workspace, "comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue("LE");
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.COMPARE:
+            var resBlock = new Blockly.BlockSvg(workspace, "three_digit_comparison");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.AND:
+            var resBlock = new Blockly.BlockSvg(workspace, "and");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.OR:
+            var resBlock = new Blockly.BlockSvg(workspace, "or");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            op1Block = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand1").connection.connect(op1Block.outputConnection);
+
+            op2Block = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("operand2").connection.connect(op2Block.outputConnection);
+
+            return resBlock
+        case ExprType.NOT:
+            var resBlock = new Blockly.BlockSvg(workspace, "not");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            opBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("operand").connection.connect(opBlock.outputConnection);
+
+            return resBlock
+        case ExprType.CHECK_REL:
+            var resBlock = new Blockly.BlockSvg(workspace, "check_relationship");
+            resBlock.initSvg();
+            resBlock.itemCount_ = 0;
+            resBlock.render();
+
+            relBlock = new Blockly.BlockSvg(workspace, "relationship");
+            relBlock.initSvg();
+            relBlock.render();
+            relBlock.inputList[0].fieldRow[0].setValue(exprNode.rel);
+            resBlock.getInput("relationship").connection.connect(relBlock.outputConnection);
+            
+            objBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            var current = exprNode.objectSeq.first;
+            while (current != null) {
+                resBlock.itemCount_++;
+                resBlock.updateShape_();
+                objOpBlock = printExprNode(current);
+                resBlock.getInput("object" + (resBlock.itemCount_-1)).connection.connect(objOpBlock.outputConnection);
+                current = current.next;
+            }
+
+            return resBlock
+        case ExprType.CHECK_VAL:
+            // var resBlock = new Blockly.BlockSvg(workspace, "check_value_of_property");
+            // resBlock.initSvg();
+            // resBlock.render();
+            
+            // objBlock = printExprNode(exprNode.firstOperand);
+            // resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            // return resBlock
+
+
+            // result = "ID" + exprNode.id + " [label=\"Check value\"]\n";
+            // result += printExprNode(exprNode.firstOperand);
+            // result += "ID" + exprNode.id +"->ID"+ exprNode.firstOperand.id +"[label=\"Property\"]\n";
+            // result += printExprNode(exprNode.secondOperand);
+            // result += "ID" + exprNode.id +"->ID"+ exprNode.secondOperand.id +"[label=\"Value\"]\n";
+            // return result;
+        case ExprType.GET_CLASS:
+            var resBlock = new Blockly.BlockSvg(workspace, "get_class");
+            resBlock.initSvg();
+            resBlock.render();
+            
+            objBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("object").connection.connect(objBlock.outputConnection);
+
+            return resBlock
+        case ExprType.FIND:
+            var resBlock = new Blockly.BlockSvg(workspace, "get_condition_object");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue(exprNode.ident);
+            
+            condBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("condition").connection.connect(condBlock.outputConnection);
+
+            return resBlock
+        case ExprType.FIND_EXTREM:
+            var resBlock = new Blockly.BlockSvg(workspace, "get_extr_object");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[1].fieldRow[0].setValue(exprNode.extremeIdent);
+            resBlock.inputList[3].fieldRow[0].setValue(exprNode.ident);
+            
+            extrCondBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("extreme_condition").connection.connect(extrCondBlock.outputConnection);
+
+            condBlock = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("general_condition").connection.connect(condBlock.outputConnection);
+
+            return resBlock
+        case ExprType.EXIST:
+            var resBlock = new Blockly.BlockSvg(workspace, "quantifier_of_existence");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[2].fieldRow[0].setValue(exprNode.ident);
+            
+            condBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("condition").connection.connect(condBlock.outputConnection);
+
+            return resBlock
+        case ExprType.FORALL:
+            var resBlock = new Blockly.BlockSvg(workspace, "quantifier_of_generality");
+            resBlock.initSvg();
+            resBlock.render();
+            resBlock.inputList[3].fieldRow[0].setValue(exprNode.ident);
+            
+            defBlock = printExprNode(exprNode.firstOperand);
+            resBlock.getInput("definition_area").connection.connect(defBlock.outputConnection);
+
+            verBlock = printExprNode(exprNode.secondOperand);
+            resBlock.getInput("verification_condition").connection.connect(verBlock.outputConnection);
+
+            return resBlock
+    }
+}
---
+++ test_site_blockly/create_nodes.js
@@ -0,0 +1,174 @@
+var LASTID = 0;
+
+function ProgramNode(statement) {
+    this.stmt = statement;
+    this.id = LASTID++;
+}
+
+function StatementNode(firstExpression, secondExpression) {
+    this.firstExpr = firstExpression;
+    this.secondExpr = secondExpression;
+    this.id = LASTID++;
+}
+
+const ExprType = { 
+    ID: 'id', 
+    STRING: 'string', 
+    INT: 'int',
+    DOUBLE: 'double',
+    BOOLEAN: 'boolean',
+    TREE_VAR: 'tree_var',
+    ENUM: 'enum',
+    GET_BY_RELATIONSHIP: 'get by relationship',
+    PROPERTY: 'property',
+    IS: 'is',
+    GREATER: 'greater', 
+    LESS: 'less', 
+    EQUAL: 'equal', 
+    NOT_EQUAL: 'not equal', 
+    GE: 'ge', 
+    LE: 'le', 
+    COMPARE: 'compare', 
+    AND: 'and', 
+    OR: 'or', 
+    NOT: 'not', 
+    CHECK_REL: 'check_rel', 
+    CHECK_VAL: 'check_val', 
+    GET_CLASS: 'get_class', 
+    FIND: 'find',
+    FIND_EXTREM: 'find extreme', 
+    EXIST: 'exist', 
+    FORALL: 'forall', 
+};
+
+function ExpressionNode() {
+    this.id = LASTID++;
+    this.type = null;
+    this.ident = null;
+    this.rel = null;
+    this.extremeIdent = null;
+    this.string = null;
+    this.int = null;
+    this.double = null;
+    this.boolean = null;
+
+    this.identValue = null;
+
+    this.firstOperand = null;
+    this.secondOperand = null;
+
+    this.objectSeq = null;
+    this.next = null;
+}
+
+function createBinExprNode(typeNode, firstExprOperand, secondExprOperand) {
+    newNode = new ExpressionNode();
+    newNode.type = typeNode;
+    newNode.firstOperand = firstExprOperand;
+    if(typeNode == ExprType.PROPERTY) {
+        newNode.ident = secondExprOperand;
+    } else {
+        newNode.secondOperand = secondExprOperand;
+    }
+    
+    return newNode;
+}
+
+function createGetObjectByRel(firstExprOperand, relationship, id, secondExprOperand) {
+    newNode = new ExpressionNode();
+    newNode.type = ExprType.GET_BY_RELATIONSHIP;
+    newNode.ident = id;
+    newNode.rel = relationship;
+
+    newNode.firstOperand = firstExprOperand;
+    newNode.secondOperand = secondExprOperand;
+    
+    return newNode;
+}
+
+function createUnaryExprNode(typeNode, operand) {
+    newNode = new ExpressionNode();
+    newNode.type = typeNode;
+    newNode.firstOperand = operand;
+    return newNode;
+}
+
+function createLiteral(typeNode, literal) {
+    newNode = new ExpressionNode();
+    newNode.type = typeNode;
+    if(typeNode == ExprType.ID || typeNode == ExprType.TREE_VAR) {
+        newNode.ident = literal;
+    } else if(typeNode == ExprType.STRING) {
+        newNode.string = literal;
+    } else if(typeNode == ExprType.INT) {
+        newNode.int = Number(literal);
+    } else if(typeNode == ExprType.DOUBLE) {
+        newNode.double = Number(literal);
+    } else if(typeNode == ExprType.BOOLEAN) {
+        newNode.boolean = literal;
+    } 
+    return newNode;
+}
+
+function createEnum(idOwner, idValue) {
+    newNode = new ExpressionNode();
+    newNode.type = ExprType.ENUM;
+    newNode.ident = idOwner;
+    newNode.identValue = idValue;
+    return newNode;
+}
+
+function createCheckRelExprNode(expression, relationship, objectSeq) {
+    newNode = new ExpressionNode();
+    newNode.type = ExprType.CHECK_REL;
+    newNode.firstOperand = expression;
+    newNode.rel = relationship;
+    newNode.objectSeq = objectSeq;
+    return newNode;
+}
+
+function createGetExprNode(typeNode, id, expression) {
+    newNode = new ExpressionNode();
+    newNode.type = typeNode;
+    newNode.firstOperand = expression;
+    newNode.ident = id;
+    return newNode;
+}
+
+function createFindExtremeExprNode(extremeVarName, extremeCondition, varName, condition) {
+    newNode = new ExpressionNode();
+    newNode.type = ExprType.FIND_EXTREM;
+    newNode.extremeIdent = extremeVarName;
+    newNode.firstOperand = extremeCondition;
+    newNode.ident = varName;
+    newNode.secondOperand = condition;
+    return newNode;
+}
+
+function createForAllExprNode(id, expression1, expression2) { 
+    newNode = new ExpressionNode();
+    newNode.type = ExprType.FORALL;
+    newNode.firstOperand = expression1;
+    newNode.secondOperand = expression2;
+    newNode.ident = id;
+    return newNode;
+}
+
+function ObjectSeq(first, last) {
+    this.first = first;
+    this.last = last;
+}
+
+function createObjectSeqNode(expr) {
+    return new ObjectSeq(expr, expr);
+}
+
+function addObjectToObjectSeqNode(seq, expr) {
+    seq.last.next = expr;
+    seq.last = expr;
+    return seq;
+}
+
+var root;
+
+var string;
