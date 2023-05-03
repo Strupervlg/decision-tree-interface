a8759a4abf7bde4b44afc350f1cfca6ae48214cd
Moskalenko Nikita <53981215+Strupervlg@users.noreply.github.com>
2023-05-03 16:40:09 +0300

    Исправлена ошибка при редактировании человекочитаемого теста у ветки

--- Plugin_draw_io/build/plugin.js
+++ Plugin_draw_io/build/plugin.js
@@ -10118,8 +10118,8 @@ Draw.loadPlugin(function (ui) {
     ui.actions.addAction('editTextInNode', function () {
         if (graph.isEnabled() && graph.getSelectionCount() == 1) {
             var selectedcell = graph.getSelectionCell();
-            if(typeof selectedcell.value != "object" && !selectedcell.edge || 
-            typeof selectedcell.value == "object" && selectedcell.value.getAttribute("type") != "START" && !selectedcell.edge) {
+            if(selectedcell.value != null && typeof selectedcell.value != "object" && selectedcell.style != "rounded=1;whiteSpace=wrap;html=1;fillColor=#e6e6e6;strokeColor=#666666;editable=0;" && !selectedcell.edge || 
+            selectedcell.value != null && typeof selectedcell.value == "object" && selectedcell.value.getAttribute("type") != "START" && !selectedcell.edge) {
                 this.editTextInNodeWindow = new EditTextInNodeWindow(selectedcell, ui, (document.body.offsetWidth - 880) / 2, 120, 900, 550);
                 this.editTextInNodeWindow.window.setVisible(true);
             }
